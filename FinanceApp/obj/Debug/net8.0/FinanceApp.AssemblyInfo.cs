3b78b225c51ce670d5ec66142a1624463cece235
sd191100 <shivamdurgude1@gmail.com>
2024-10-21 08:32:01 +0530

    added photo

--- FinanceApp/obj/Debug/net8.0/FinanceApp.AssemblyInfo.cs
+++ FinanceApp/obj/Debug/net8.0/FinanceApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d16b0ad9ab69144fa210cce5e44a5505d51577f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab0cc45170e6bf9313f01d6b441f30a6b18ca4d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
